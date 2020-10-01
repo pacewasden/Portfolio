cdc7ce65ffcc231c9a4515f72ab4dc37f60ad537
Pace Wasden <pacewasden@gmail.com>
2020-09-30 21:26:45 -0600

    Saving before continuing on

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Contact.apps.ContactConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
