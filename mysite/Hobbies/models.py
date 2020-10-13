1ad4a9e00c582b3681aaad633c410cf58c96ffbf
Pace Wasden <pacewasden@gmail.com>
2020-10-12 23:12:11 -0600

    This is making sure that I am saving my work

--- mysite/Hobbies/models.py
+++ mysite/Hobbies/models.py
@@ -6,6 +6,7 @@ class hobbies(models.Model):
     def __str__(self):
         return self.hobbies_name
 
-
+#change hobbie_ photo to hobbies_photo
     hobbies_name=models.CharField(max_length=200)
     hobbies_type=models.CharField(max_length=200)
+    hobbie_photo=models.CharField(max_length=500, default="https://i.ytimg.com/vi/Tz7EwqQFhjk/maxresdefault.jpg")
